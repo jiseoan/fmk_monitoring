3a4fe502c4fe1db26656788825872b32b6267da9
cheolhong <popodaddy@gmail.com>
2019-11-14 12:56:22 +0900

    사진 촬영 기능 추가.

--- app/src/main/java/com/segeuru/soft/monitering/CameraViewer.java
+++ app/src/main/java/com/segeuru/soft/monitering/CameraViewer.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.ImageFormat;
 import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
@@ -12,22 +13,44 @@ import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureFailure;
 import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.TotalCaptureResult;
 import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.Image;
+import android.media.ImageReader;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.util.Log;
 import android.util.Size;
+import android.util.SparseIntArray;
 import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class CameraViewer extends AppCompatActivity {
+
+    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0, 90);
+        ORIENTATIONS.append(Surface.ROTATION_90, 0);
+        ORIENTATIONS.append(Surface.ROTATION_180, 270);
+        ORIENTATIONS.append(Surface.ROTATION_270, 180);
+    }
 
     private static final String DEBUG_TAG = "segeuru.com";
     private String m_cameraId;
@@ -35,8 +58,6 @@ public class CameraViewer extends AppCompatActivity {
     private CameraDevice m_cameraDevice;
     private CaptureRequest.Builder m_captureRequestBuilder;
     private CameraCaptureSession m_captureSession;
-    private Handler m_handler;
-    private HandlerThread m_handlerThread;
 
     private TextureView m_textureViewer;
     private Button m_btnTakePicture;
@@ -57,7 +78,7 @@ public class CameraViewer extends AppCompatActivity {
         m_btnTakePicture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                takePicture();
             }
         });
     }
@@ -120,7 +141,105 @@ public class CameraViewer extends AppCompatActivity {
 
         try {
             m_captureRequestBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
-            m_captureSession.setRepeatingRequest(m_captureRequestBuilder.build(), null, m_handler);
+            m_captureSession.setRepeatingRequest(m_captureRequestBuilder.build(), null, null);
+        } catch(CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void takePicture() {
+        Log.d(DEBUG_TAG, "take a picture");
+        if(null == m_cameraDevice) {
+            Log.e(DEBUG_TAG, "cemeraDevuce is null");
+            return;
+        }
+
+        CameraManager manager = (CameraManager)getSystemService(Context.CAMERA_SERVICE);
+
+        try {
+            CameraCharacteristics characteristics = manager.getCameraCharacteristics(m_cameraDevice.getId());
+            Size[] jpegSizes = null;
+            if(null != characteristics) {
+                jpegSizes = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP).getOutputSizes(ImageFormat.JPEG);
+                Log.d(DEBUG_TAG, Arrays.toString(jpegSizes));
+            }
+
+            int width = 640;
+            int height = 480;
+            if (jpegSizes != null && 0 < jpegSizes.length) {
+                width = jpegSizes[0].getWidth();
+                height = jpegSizes[0].getHeight();
+            }
+
+            final ImageReader reader = ImageReader.newInstance(width, height, ImageFormat.JPEG, 1);
+            List<Surface> outputSurface = new ArrayList<>(2);
+            outputSurface.add(reader.getSurface());
+            outputSurface.add(new Surface(m_textureViewer.getSurfaceTexture()));
+            final CaptureRequest.Builder captureBuilder = m_cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
+            captureBuilder.addTarget(reader.getSurface());
+            captureBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
+
+            //orientation
+            int rotation = getWindowManager().getDefaultDisplay().getRotation();
+            Log.d(DEBUG_TAG, String.format("device rotaion is %d", rotation));
+
+            captureBuilder.set(CaptureRequest.JPEG_ORIENTATION, ORIENTATIONS.get(rotation));
+            final File file = new File(Environment.getExternalStorageDirectory() + "/pic.jpg");
+            ImageReader.OnImageAvailableListener readerListener = new ImageReader.OnImageAvailableListener() {
+                @Override
+                public void onImageAvailable(ImageReader imageReader) {
+                    Image image = null;
+                    try {
+                        image = reader.acquireLatestImage();
+                        ByteBuffer buffer = image.getPlanes()[0].getBuffer();
+                        byte[] bytes = new byte[buffer.capacity()];
+                        buffer.get(bytes);
+                        save(bytes);
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                        if(null != image) image.close();
+                    }
+                }
+
+                public void save(byte[] bytes) throws IOException {
+                    OutputStream output = null;
+                    try {
+                        output = new FileOutputStream(file);
+                        output.write(bytes);
+                    } finally {
+                        if(null != output) output.close();
+                    }
+                }
+            };
+            reader.setOnImageAvailableListener(readerListener, null);
+
+            final CameraCaptureSession.CaptureCallback captureCallback = new CameraCaptureSession.CaptureCallback() {
+                @Override
+                public void onCaptureCompleted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull TotalCaptureResult result) {
+                    super.onCaptureCompleted(session, request, result);
+                    Toast.makeText(CameraViewer.this, "Saved:" + file, Toast.LENGTH_SHORT).show();
+                    createCameraPreview(); //<------------------------???????
+                }
+            };
+            m_cameraDevice.createCaptureSession(outputSurface, new CameraCaptureSession.StateCallback() {
+                @Override
+                public void onConfigured(@NonNull CameraCaptureSession session) {
+                    try {
+                        session.capture(captureBuilder.build(), captureCallback, null);
+                    } catch (CameraAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+
+                }
+            }, null);
+
+
         } catch(CameraAccessException e) {
             e.printStackTrace();
         }
--- app/src/main/java/com/segeuru/soft/monitering/MainActivity.java
+++ app/src/main/java/com/segeuru/soft/monitering/MainActivity.java
@@ -10,6 +10,8 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -50,6 +52,7 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
             finish();
         }
 
+        Log.d("segeuru.com", MediaStore.Images.Media.EXTERNAL_CONTENT_URI.toString());
     }
 
     private boolean hasPermissions(String[] permissions) {
