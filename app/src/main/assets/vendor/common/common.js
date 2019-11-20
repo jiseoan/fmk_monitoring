c50e8fe0ca4960c501948288215b7ca04b14727b
ssuyaKim <ssuya@falsto.com>
2019-11-20 19:57:30 +0900

    예전 업로드 방식 전달

--- app/src/main/assets/vendor/common/common.js
+++ app/src/main/assets/vendor/common/common.js
@@ -77,3 +77,85 @@ function dbSelect(field, table, where, orderBy)
   result = dbSql(query);
   return result;
 }
+
+var __ajaxSkinList = {};
+
+function addAjaxSkin(skin_dom, replace_text)
+{
+  var domHtml = $(skin_dom).html();
+  if (typeof(__ajaxSkinList[skin_dom]) === "undefined") // 등록되어 있지 않은 템플릿이면 넣어줌
+  {
+    __ajaxSkinList[skin_dom] = domHtml;
+    if (typeof(replace_text) !== "undefined")
+    {
+      $(skin_dom).html(replace_text);
+    }
+  }
+  else
+  {
+    domHtml = getAjaxSkin(skin_dom);
+  }
+  return domHtml;
+}
+
+
+function getAjaxSkin(skin_dom)
+{
+  if (typeof(__ajaxSkinList[skin_dom]) !== "undefined")
+  {
+    return __ajaxSkinList[skin_dom];
+  }
+  return "";
+}
+
+function replaceDom(skin, param, delimiter)
+{
+  //console.log("replaceDom " + skin);
+  if (typeof(delimiter) === "undefined")
+    delimiter = "[]";
+  var newSkin = "";
+  //console.log(param);
+  if (param != null && typeof(param) === "object") // 2차원 배열-오브젝트
+  {
+    if (param.length > 0)
+    {
+      if (typeof(param[0]) === "object")
+      {
+        for (var key in param) 
+        {
+          var replacedText = skin;
+          for (var key2 in param[key])
+          {
+            //replacedText = replaceAll(replacedText, delimiter.substr(0, 1) + key2 + delimiter.substr(1, 1), param[key][key2]);
+            var regEx = new RegExp("\\" + delimiter.substr(0, 1) + key2 + "\\" + delimiter.substr(1, 1), "g");
+            replacedText = String(replacedText).replace(regEx, param[key][key2]);
+          }
+          
+          replacedText = String(replacedText).replace(/tag=/g, 'src=');
+
+          newSkin += replacedText;
+        }
+      }
+    } 
+    else // 1차원 오브젝트
+    {
+      newSkin = skin;
+      if (typeof(param.length) === "undefined" && typeof(param) === "object") // 배열이 아닌 오브젝트라면
+      {
+        for (var key in param) 
+        {
+          // newSkin = replaceAll(newSkin, delimiter.substr(0, 1) + key + delimiter.substr(1, 1), param[key]);
+          var regEx = new RegExp("\\" + delimiter.substr(0, 1) + key + "\\" + delimiter.substr(1, 1), "g");
+          newSkin = String(newSkin).replace(regEx, param[key]);
+        }              
+        newSkin = String(newSkin).replace(/tag=/g, 'src=');
+      }
+      else // 배열이면서 length가 0일 경우
+      {
+        newSkin = "";
+      }
+    }   
+  }
+
+  return newSkin;
+}
