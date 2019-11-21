8cd86fc3acd3d488ad726b2ee61e4e66a9fba7a2
ssuyaKim <ssuya@falsto.com>
2019-11-21 18:15:07 +0900

    페이지 구성 변경
    #. main의 기능들을 test로 이동, webview 첫화면 test로 나오게 수정

--- app/src/main/assets/vendor/common/common.js
+++ app/src/main/assets/vendor/common/common.js
@@ -31,6 +31,43 @@ function getPlatform()
   return platform;
 }
 
+function callNative(command, param) {
+  var result  = null;
+  if (typeof(param) === "undefined") param = "";
+  var checkOS = getPlatform();
+  try {
+    if (checkOS == "Android") {
+      if (command == "deviceUniq") {
+        // 디바이스 고유값 가져오기
+        result = window.android.deviceUniq();
+      } else if (command == "bottomActionBar") {
+        // 하단바 레이아웃별 보이기
+        // bottom_actionbar_A1 : 작업시작 (QR코드스킨 열기)
+        // bottom_actionbar_A2 : 작업시작 (카메라 열기 - 모니터링용) | 취소
+        // bottom_actionbar_A3 : 재촬영 | AS요청 | 확인
+        // bottom_actionbar_B1 : 목록
+        // bottom_actionbar_B2 : 등록 | 취소
+        // bottom_actionbar_C1 : AS처리 재등록
+        // bottom_actionbar_C2 : 재촬영 | 확인
+        // bottom_actionbar_D1 : AS처리등록
+        // bottom_actionbar_D2 : 완료 | 취소
+
+        window.android.bottomActionBar(param, true);
+      } else if (command == "toolBar") {
+        // 상단바 레이아웃별 보이기
+        // top_actionbar_A1 : 메인 상단바 열기
+        window.android.toolBar(param, true);
+      }
+    } else {
+      console.log("ignore callNative('" + command + "', '" + param + "')");
+    }
+  } catch (e) { 
+    console.log("ignore callNative('" + command + "', '" + param + "')");
+  }
+
+  return result;
+}
+
 function dbSql(query) {
   var result = null;
   console.log(query);
