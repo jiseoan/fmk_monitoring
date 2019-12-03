acfe60f9092cd0639ed3171fe46ff37127f82a0e
cheolhong <popodaddy@gmail.com>
2019-12-03 13:40:10 +0900

    이미지 파일 멀티 업로드 기능 개선.

--- app/src/main/java/com/segeuru/soft/monitering/WebSupport.java
+++ app/src/main/java/com/segeuru/soft/monitering/WebSupport.java
@@ -10,83 +10,103 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class WebSupport {
     private final String DEBUG_TAG = "segeuru.com";
     private Context m_context;
+    private ArrayList<String> m_fileNames;
+
+    public WebSupport() {
+        m_fileNames = new ArrayList<>();
+        m_fileNames.add("040.jpg");
+        m_fileNames.add("045.jpg");
+    }
 
     private void uploadImage(Activity activity) {
         final String SERVER_URL = "http://test.raonworks.com/upload.php";
         final String boundary = "*****";
         final String twoHyphens = "--";
         final String lineEnd = "\r\n";
-        final String fileName = "a.jpg";
+        //final String fileName = "040.jpg";
 
         URL url = null;
         byte[] buffer;
         int bytesRead, bytesAvailable, bufferSize;
         int maxBufferSize = 1024 * 1024;
         HttpURLConnection httpConn = null;
-        DataOutputStream dos = null;
 
-        File sourceFile = new File(MoniteringApp.APP_STORE_PATH + "/" + fileName);
-        if(!sourceFile.exists()) {
-            Log.i(DEBUG_TAG, "not exists");
-            return;
-        }
+//        File sourceFile = new File(MoniteringApp.APP_STORE_PATH + "/" + fileName);
+//        if(!sourceFile.exists()) {
+//            Log.i(DEBUG_TAG, "not exists");
+//            return;
+//        }
 
         try {
-            FileInputStream fileInputStream = new FileInputStream(sourceFile);
-            url = new URL(SERVER_URL);
-
             // Open a HTTP  connection to  the URL
+            url = new URL(SERVER_URL);
             httpConn = (HttpURLConnection) url.openConnection();
             httpConn.setDoInput(true); // Allow Inputs
             httpConn.setDoOutput(true); // Allow Outputs
             httpConn.setUseCaches(false); // Don't use a Cached Copy
             httpConn.setRequestMethod("POST");
             httpConn.setRequestProperty("Connection", "Keep-Alive");
+            httpConn.setRequestProperty("Charset", "UTF-8");
             httpConn.setRequestProperty("ENCTYPE", "multipart/form-data");
             httpConn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
-            httpConn.setRequestProperty("uploaded_file", fileName);
+            //httpConn.setRequestProperty("uploaded_file", fileName);
 
-            dos = new DataOutputStream(httpConn.getOutputStream());
+            DataOutputStream dos = new DataOutputStream(httpConn.getOutputStream());
             dos.writeBytes(twoHyphens + boundary + lineEnd);
-            dos.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\"" + fileName + "\"" + lineEnd);
-            dos.writeBytes(lineEnd);
 
-            // create a buffer of  maximum size
-            bytesAvailable = fileInputStream.available();
+            for(int i=0;i<m_fileNames.size();++i) {
+                dos.writeBytes("Content-Disposition: form-data; name=\"uploaded_file[]\";filename=\"" + m_fileNames.get(i) + "\"" + lineEnd);
+                dos.writeBytes(lineEnd);
 
-            bufferSize = Math.min(bytesAvailable, maxBufferSize);
-            buffer = new byte[bufferSize];
+                File sourceFile = new File(MoniteringApp.APP_STORE_PATH + "/" + m_fileNames.get(i));
+                if (!sourceFile.exists()) {
+                    Log.i(DEBUG_TAG, "not exists");
+                    return;
+                }
+                FileInputStream fileInputStream = new FileInputStream(sourceFile);
 
-            // read file and write it into form...
-            bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-
-            while (bytesRead > 0) {
-                dos.write(buffer, 0, bufferSize);
+                // create a buffer of  maximum size
                 bytesAvailable = fileInputStream.available();
+
                 bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                buffer = new byte[bufferSize];
+
+                // read file and write it into form
                 bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                while (bytesRead > 0) {
+                    dos.write(buffer, 0, bufferSize);
+                    bytesAvailable = fileInputStream.available();
+                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+                }
+
+                // send multipart form data necesssary after file data...
+                fileInputStream.close();
+                dos.writeBytes(lineEnd);
+                dos.writeBytes(twoHyphens + boundary + lineEnd);
+                //dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
             }
 
-            // send multipart form data necesssary after file data...
-            dos.writeBytes(lineEnd);
             dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
 
-            // Responses from the server (code and message)
-            int serverResponseCode = httpConn.getResponseCode();
-            String serverResponseMessage = httpConn.getResponseMessage();
+                // Responses from the server (code and message)
+                int serverResponseCode = httpConn.getResponseCode();
+                String serverResponseMessage = httpConn.getResponseMessage();
 
-            Log.i("uploadFile", "HTTP Response is : "
-                    + serverResponseMessage + ": " + serverResponseCode);
+                Log.i("uploadFile", "HTTP Response is : "
+                        + serverResponseMessage + ": " + serverResponseCode);
 
-            if(serverResponseCode == 200){
-
-                activity.runOnUiThread(new Runnable() {
-                    public void run() {
-
+//                if (serverResponseCode == 200) {
+//
+//                    activity.runOnUiThread(new Runnable() {
+//                        public void run() {
+//
 //                        String msg = "File Upload Completed.\n\n See uploaded file here : \n\n"
 //                                +" http://www.androidexample.com/media/uploads/"
 //                                +uploadFileName;
@@ -94,14 +114,13 @@ public class WebSupport {
 //                        messageText.setText(msg);
 //                        Toast.makeText(UploadToServer.this, "File Upload Complete.",
 //                                Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
+//                        }
+//                    });
+//                }
 
-            //close the streams //
-            fileInputStream.close();
-            dos.flush();
-            dos.close();
+                //close the streams //
+                dos.flush();
+                dos.close();
 
 //            int responseCode = httpConn.getResponseCode();
 //            Log.i("segeuru.com", Integer.toString(responseCode));
